86056326a65276236707d9df6a055f1e2fe976a5
Julie Nguyen <nguy0856@algonquinlive.com>
2025-05-12 10:58:38 -0400

    Initial Commit.

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _counter = 0.0;
+  var myFontSize = 30.0;
+
+  void setNewValue(double value) {
+    setState(() {
+      myFontSize = value;
+    });
+  }
+  void _incrementCounter() {
+    setState(() {
+      if(_counter < 100.0)
+        _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('You can change the size of this font with the slider.', style:TextStyle(fontSize:myFontSize)),
+            Text(
+              '$myFontSize',
+              style: TextStyle(fontSize:myFontSize),
+            ),
+            Slider(value: myFontSize, max: 100.0, onChanged: setNewValue, min:0.0)
+          ],
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ),
+    );
+  }
+}
