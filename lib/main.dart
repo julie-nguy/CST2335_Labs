875e404bcb4727825419cf67caa73e3b643454a4
Julie Nguyen <julie.ngu03@gmail.com>
2025-06-04 21:59:31 -0400

    Completed lab 4.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Lab 4',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Lab 4'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -35,6 +37,9 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
+    EncryptedSharedPreferences prefs = EncryptedSharedPreferences();
+    getPreferences();
+
     _loginController = TextEditingController();
     _passController = TextEditingController();
   }
@@ -56,6 +61,32 @@ class _MyHomePageState extends State<MyHomePage> {
       setState(() {
         imageSource = "images/stop.png";
       });
+    }
+  }
+
+  //get encrypted preferences for the application
+  void getPreferences() async {
+    EncryptedSharedPreferences prefs = EncryptedSharedPreferences();
+    var login = await prefs.getString("Login");
+    var pass = await prefs.getString("Password");
+
+    if(login.isNotEmpty && pass.isNotEmpty) {
+      _loginController.text = login;
+      _passController.text = pass;
+      Future.delayed(Duration.zero, () {
+          var snackBar = SnackBar(
+              content: Text("Your login credentials have been saved."),
+              duration: const Duration(minutes: 2),
+              action: SnackBarAction(
+                  label: "Got it!",
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                  }
+              )
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        }
+      );
     }
   }
 
@@ -83,7 +114,36 @@ class _MyHomePageState extends State<MyHomePage> {
             Padding(
               padding: EdgeInsets.all(10.0),
               child: ElevatedButton(
-                onPressed: buttonClicked,
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext ctx)
+                    {
+                      return AlertDialog(
+                        title: Text("Would you like to save your username and password?"),
+                        actions: [
+                          FilledButton(
+                            onPressed: (){
+                              EncryptedSharedPreferences prefs = EncryptedSharedPreferences();
+                              prefs.setString("Login", _loginController.value.text); //save login to encrypted preferences
+                              prefs.setString("Password", _passController.value.text); //save password to encrypted preferences
+                              Navigator.pop(ctx); //close the pop-up
+                            },
+                            child: Text("Yes"),
+                          ),
+                          OutlinedButton(
+                            onPressed: (){
+                              EncryptedSharedPreferences prefs = EncryptedSharedPreferences();
+                              prefs.clear(); //remove saved data
+                              Navigator.pop(ctx);
+                            },
+                            child: Text("No"),
+                          ),
+                        ],
+                      );
+                    }
+                  );
+                },
                 child: Text("Login", style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold, color: Colors.blue)),
               ),
             ),
